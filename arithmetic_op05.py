fd6d2e748b02a68ccf3819e6f458a29e4af4ac51
asrorbobomurodov <mr.aceonejr777@gmail.com>
2023-07-11 20:33:15 +0500

    fifth task

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,5 +1,6 @@
 #Create a variable called 'number' with data type int.
-
+number = 56
 #Create a variable called 'answer' and assign the remainder of the division of number by 3 to it.
-
+answer = 56%3
 #Print 'answer'.
+print(answer)
